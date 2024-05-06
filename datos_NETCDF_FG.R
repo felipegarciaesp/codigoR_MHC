da845a8dcbcfdd255965c70f06cea061406716f6
Felipe Garcia <felipegarcia.esp@gmail.com>
2024-05-06 07:25:36 -0400

    lectura de datos de precipitacion y coordenadas de interes en el NetCDF

--- datos_NETCDF_FG.R
+++ datos_NETCDF_FG.R
@@ -9,7 +9,7 @@ graphics.off()
 library(ncdf4)
 
 # Definicion directorio de trabajo
-setwd("C:/Users/Usuario/Codigos_R/datos_NETCDF")
+setwd("C:/Users/Usuario/Codigos_R/leer_datos_NETCDF")
 getwd()
 
 # Importar archivos NetCDF (los GCM).
@@ -23,3 +23,42 @@ pr <- nc_open(name)
 # Comando para visualizar el archivo NetCDF que se ha importado:
 View(pr)
 
+# Comando que muestra en Consola los atributos dentro de la variable "pr"
+# de tipo NetCDF.
+print(pr)
+
+# Es importante explorar los archivos NetCDF, asi como tambien es importante
+# cerrar el archivo NetCDF.
+
+# Con "ncvar_get" se extraen valores de la variable de interes:
+data <- ncvar_get(pr)
+
+# Con "$" se puede extraer la informacion de la variable en cuestion:
+print(pr$dim$lat)
+
+# Con el comando anterior tenemos que los valores van desde -89.375
+# hasta 89.375. Esto es, desde el Polo Sur hasta el Polo Norte.
+# -89.375 quiere decir 89.375°S.
+
+# Con rm() se borran las variables que ya no se van a utilizar. En nuestro
+# caso, los valores de pr ya fueron pasados a la variable data.
+rm(pr)
+
+# Importamos los valores del GCM futuro:
+pr2 <- nc_open(name2)
+
+# Pasamos los valores que nos interesa utilizar a la variable data2:
+data2 <- ncvar_get(pr2)
+
+# Se guadan los valores de latitud y longitud del punto que queremos
+# estudiar (Rio Cauquenes En Desembocadura, en este caso).
+
+QNlat <- (-35.90)
+
+# Queremos encontrar las coordenadas del NNetCDF que tienen esa latitud y
+# longitud.
+
+# Nos quedamos con la mayor coordenada dentro del archivo NetCDF que sea
+# menos a la del punto de estudio.
+
+corLat <- max(which(pr2$dim$lat$vals<QNlat))
