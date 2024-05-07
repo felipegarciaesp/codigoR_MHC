584ff15f10ef0bb4fb7be11a9ee9f7e7d41ca2e4
Felipe Garcia <felipegarcia.esp@gmail.com>
2024-05-07 19:09:32 -0400

    Encuentro de coordenadas a estudiar en el GCM

--- datos_NETCDF_FG.R
+++ datos_NETCDF_FG.R
@@ -53,12 +53,51 @@ data2 <- ncvar_get(pr2)
 # Se guadan los valores de latitud y longitud del punto que queremos
 # estudiar (Rio Cauquenes En Desembocadura, en este caso).
 
-QNlat <- (-35.90)
+latitud <- -35.90
+longitud <- -72.05
+
+QNlat <- latitud
 
 # Queremos encontrar las coordenadas del NNetCDF que tienen esa latitud y
 # longitud.
+
+# =====================================================================
+# Se puede hacer una funcion de lo que viene ahora (respecto a corLat y corLon)
+# Revisa bien si el if que hace para determinar el verdadero indice esta correcto,
+# independiene si QNlat es positivo o negativo.
 
 # Nos quedamos con la mayor coordenada dentro del archivo NetCDF que sea
 # menos a la del punto de estudio.
 
 corLat <- max(which(pr2$dim$lat$vals<QNlat))
+
+# corLat es por ende la latitud justo menor a las coordenadas de la estación
+# Debe notarse que corLat es un resultado que indica un indice dentro de pr2$dim$lat$vals.
+
+# En el siguiente if se compara la distancia entre QNLat, con el valor de latitudes del GCM y se verifica
+# si la latitud justo menor a la más cercana o la más cercana es la justo mayor. De ser la mayor le
+# suma 1 a la coordenada de corLat
+if ((QNlat-pr2$dim$lat$vals[corLat])>(pr2$dim$lat$vals[corLat+1]-QNlat)){
+  corLat=corLat+1}
+
+# El objetivo de este if es que verifica si el punto de la grilla más cercano es el justo menor a la
+# coordenada que estamos buscando, la cual tenemos guardada en corLat, de no ser asi, asume que la
+# coordenada es la justo mayor
+
+# Repetimos el proceso para la longitud.
+# cuidado, con las coordenadas de la longitud, los GCM consideran longitud Este, por lo que para
+# introducir la longitud en el caso que sea negativo (-70°), hay que restarle dicho valor a los
+# 360° del planeta.
+QNlon<-(360+longitud)
+
+corLon<-max(which(pr2$dim$lon$vals<QNlon))
+if ((QNlon-pr2$dim$lon$vals[corLon])>(pr2$dim$lon$vals[corLon+1]-QNlon)){
+  corLon=corLon+1}
+
+# =====================================================================
+
+# Una vez que se encuentran las coordenadas más cercanas a la estación en el NetCDF,
+# puedo seleccionar dichas coordenadas, trabajar con ellas y olvidarme del resto del
+# archivo. OJO CON TRANSFORMAR UNIDADES!
+
+
